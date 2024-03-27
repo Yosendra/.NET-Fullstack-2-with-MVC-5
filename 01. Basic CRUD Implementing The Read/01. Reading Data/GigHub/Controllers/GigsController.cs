671d718796d8a9870498af145ddb0ecefc887f22
Yosendra <yosendra.evri@gmail.com>
2024-03-27 19:59:01 +0800

    Add some minor enchancements

--- 01. Basic CRUD Implementing The Read/01. Reading Data/GigHub/Controllers/GigsController.cs
+++ 01. Basic CRUD Implementing The Read/01. Reading Data/GigHub/Controllers/GigsController.cs
@@ -80,7 +80,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
--- 01. Basic CRUD Implementing The Read/01. Reading Data/GigHub/Global.asax.cs
+++ 01. Basic CRUD Implementing The Read/01. Reading Data/GigHub/Global.asax.cs
@@ -14,10 +14,14 @@ using System.Web.Routing;
  * Add a view for that action method named "Mine.cshtml"
  * Add one more menu "My Upcoming Gigs" to view my gig page and put hyperlink to that page
  * 
+ * Currently, when user add a gig, user will be directed to home page
+ * Now we will change it to direct to My Upcoming Gigs page
+ * On GigsController.Create(), change the .RedirectToAction() to Mine() action method of GigsController
+ * 
  * Test: login then access the "My Upcoming Gigs" dropdown menu
  * 
  * Look at:
- * • GigsController.cs -> Mine(), Main.cshtml
+ * • GigsController.cs -> Mine(), Main.cshtml, Create() (POST)
 */
 
 namespace GigHub
