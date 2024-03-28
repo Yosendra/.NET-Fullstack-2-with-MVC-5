a647d9d5b5ab978dfe902baf0077a0b73cdcec4b
Yosendra <yosendra.evri@gmail.com>
2024-03-28 22:17:26 +0800

    Basic CRUD Implementing The Delete - 08. Handling Edge Cases

---
+++ 03. Basic CRUD Implementing The Delete/08. Handling Edge Cases/GigHub/Global.asax.cs
@@ -0,0 +1,31 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * We will fix a logical error in GigsController (API) at Cancel()
+ * We forgot to put guarding logic if a particular gig is already canceled, 
+ * when we cancel the same gig again, it will response with not found
+ * 
+ * Look at:
+ * • GigsController.cs (API) -> Cancel()
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
