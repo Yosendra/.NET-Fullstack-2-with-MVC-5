91794bd40aefecebe7683288b9b108509d31a402
Yosendra <yosendra.evri@gmail.com>
2024-03-28 17:42:36 +0800

    Basic CRUD Implementing The Update - 05. Using Expressions to Replace Magic Strings

---
+++ 02. Basic CRUD Implementing The Update/05. Using Expressions to Replace Magic Strings/GigHub/Global.asax.cs
@@ -0,0 +1,30 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * In GigFormVM we use raw string "Update" & "Create" for assigned value to Action property
+ * We will refactor this using Expression syntax (it is quite complex, let just see the syntax at GigFormVM)
+ * 
+ * Look at:
+ * • GigFormVM.cs
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
