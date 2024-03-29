33e27f805b82411a964edce9a726cfbb7cd3905e
Yosendra <yosendra.evri@gmail.com>
2024-03-29 16:12:28 +0800

    Refactoring Towards an Object Oriented Design - 05. Fixing Reverse Relationships Issue

---
+++ 05. Refactoring Towards an Object Oriented Design/05. Fixing Reverse Relationships Issue/GigHub/Global.asax.cs
@@ -0,0 +1,30 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * We need add some entity configuration of Attendance & UserNotification for reverse relationship in ApplicationDbContext
+ * Just watch, many steps...
+ * 
+ * Look at:
+ * • ApplicationDbContext.cs
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
