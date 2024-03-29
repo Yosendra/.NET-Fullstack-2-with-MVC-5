22301853a9df746c62b52df9320914c5f599c4f3
Yosendra <yosendra.evri@gmail.com>
2024-03-29 16:36:41 +0800

    Refactoring Towards an Object Oriented Design - 06. Notifications Update Feature

---
+++ 05. Refactoring Towards an Object Oriented Design/06. Notifications Update Feature/GigHub/Global.asax.cs
@@ -0,0 +1,31 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * Exercise: Implement Notifications Update Feature
+ * Just watch, many steps...
+ * 
+ * Look at:
+ * • GigsController.cs -> Edit() (POST)
+ * • Gig.cs -> Modify()
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
