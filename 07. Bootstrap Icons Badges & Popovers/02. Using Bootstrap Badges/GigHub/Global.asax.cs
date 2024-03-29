7a09d1185667d8bfcbe1f026c2193ed89f6701c2
Yosendra <yosendra.evri@gmail.com>
2024-03-30 01:11:35 +0800

    Bootstrap Icons Badges & Popovers - 02. Using Bootstrap Badges

---
+++ 07. Bootstrap Icons Badges & Popovers/02. Using Bootstrap Badges/GigHub/Global.asax.cs
@@ -0,0 +1,35 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Add badge icon at notification globe icon in navigation
+ * Add some styling for that badge
+ * 
+ * Look at:
+ * • _LoginPartial.cshtml
+ * • Site.css
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
