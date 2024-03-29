f84ea48448783b434171a585d3763bc9cafae2e3
Yosendra <yosendra.evri@gmail.com>
2024-03-29 15:38:01 +0800

    Refactoring Towards an Object Oriented Design - 03. Optimization

---
+++ 05. Refactoring Towards an Object Oriented Design/03. Optimization/GigHub/Global.asax.cs
@@ -0,0 +1,32 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+/* user1@domain.com
+ * yosi@domain.com
+ * User1#123
+ * 
+ * GigsController.Cancel() (API) we do some optimization querying database
+ * Before we do twice, now we use eager loading, so we only querying database once
+ * Just watch, many steps
+ *   
+ * Look at:
+ * • GigsController.cs (API) -> Cancel()
+ * • Gig.cs
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
