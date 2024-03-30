65b5595bee7dc6877cac766d42a50a26edce2d6c
Yosendra <yosendra.evri@gmail.com>
2024-03-31 04:26:49 +0800

    Bootstrap Icons Badges & Popovers - 07. Rendering Content Using Underscore Templates

---
+++ 07. Bootstrap Icons Badges & Popovers/07. Rendering Content Using Underscore Templates/GigHub/Global.asax.cs
@@ -0,0 +1,36 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * We try to use underscore templating
+ * Look at _Layout.cshtml
+ * 
+ * Test : Login as Artist 2, then click the notifications icon
+ * 
+ * Look at:
+ * • _Layout.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
