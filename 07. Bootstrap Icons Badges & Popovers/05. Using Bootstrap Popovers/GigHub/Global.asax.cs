8af50ef6d618c176a2713d453e320e210209e1db
Yosendra <yosendra.evri@gmail.com>
2024-03-30 22:24:33 +0800

    Bootstrap Icons Badges & Popovers - 05. Using Bootstrap Popovers

---
+++ 07. Bootstrap Icons Badges & Popovers/05. Using Bootstrap Popovers/GigHub/Global.asax.cs
@@ -0,0 +1,39 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Use Bootstrap popover at Notifications icon
+ * Read at this for more detail:
+ * https://getbootstrap.com/docs/5.3/components/popovers/
+ * 
+ * At Layout.cshtml add the javascript logic for popover effect on notification icon
+ * 
+ * Test: click on notification icon
+ * 
+ * Look at:
+ * • _Layout.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ 07. Bootstrap Icons Badges & Popovers/05. Using Bootstrap Popovers/GigHub/Scripts/_references.js
@@ -0,0 +1,3 @@
+﻿///<reference path="jquery/jquery-3.7.1.js" />
+///<reference path="jquery/jquery-3.7.1.intellisense.js" />
+///<reference path="bootstrap/bootstrap.js" />
