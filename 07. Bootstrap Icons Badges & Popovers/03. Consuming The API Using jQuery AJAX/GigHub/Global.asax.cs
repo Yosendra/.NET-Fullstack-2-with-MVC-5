b41df7255454b2df0f33fca1d7c6e973ad55e93b
Yosendra <yosendra.evri@gmail.com>
2024-03-30 09:00:01 +0800

    Bootstrap Icons Badges & Popovers - 03. Consuming The API Using jQuery AJAX

---
+++ 07. Bootstrap Icons Badges & Popovers/03. Consuming The API Using jQuery AJAX/GigHub/Global.asax.cs
@@ -0,0 +1,36 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Make the notification count badge dynamic
+ * Add class to the notification badge at _LoginPartial.cshtml
+ * Add javascript logic at _Layout.cshtml to hit API endpoint get notification to count the notifications and render the badge notification count
+ * 
+ * Look at:
+ * • _LoginPartial.cshtml
+ * • _Layout.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
