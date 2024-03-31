103cb5d716e3ab82f6d0d6612639269ad30c0c10
Yosendra <yosendra.evri@gmail.com>
2024-03-31 16:42:29 +0800

    Bootstrap Icons Badges & Popovers - 01. Improving The Look & Feel of Popover

---
+++ 08. Improving The Look & Feel/01. Improving The Look & Feel of Popover/GigHub/Global.asax.cs
@@ -0,0 +1,38 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Currently notification is in plain style also there is bullet for each notification item
+ * We will be styling it in css
+ * Look at the code
+ * 
+ * Test: login as artist 2, click on notification icon
+ * 
+ * Look at:
+ * • _Layout.cshtml
+ * • Site.css
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
