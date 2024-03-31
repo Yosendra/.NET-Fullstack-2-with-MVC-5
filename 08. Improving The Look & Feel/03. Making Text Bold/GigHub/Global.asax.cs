b46fdf66d3f8b4d792c7757074871d272f462fa2
Yosendra <yosendra.evri@gmail.com>
2024-04-01 00:25:37 +0800

    Improving The Look & Feel - 03. Making Text Bold

---
+++ 08. Improving The Look & Feel/03. Making Text Bold/GigHub/Global.asax.cs
@@ -0,0 +1,38 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * We want to highlight the Artist's name in the notification
+ * Look at _Layout.cshtml.
+ * In site.css, we give the styling for the highlight class
+ * 
+ * Test: login as artist 2, click on notification icon, the artist name become bold
+ * 
+ * Look at:
+ * • _Layout.cshtml
+ * • Site.css
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
