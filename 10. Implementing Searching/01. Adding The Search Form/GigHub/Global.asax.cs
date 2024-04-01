faed5f92bd69330fd9305313e41b684013198e79
Yosendra <yosendra.evri@gmail.com>
2024-04-01 18:37:53 +0800

    Implementing Searching - 01. Adding The Search Form

---
+++ 10. Implementing Searching/01. Adding The Search Form/GigHub/Global.asax.cs
@@ -0,0 +1,37 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Adding search form in Gigs.cshtml
+ * Add property SearchTerm in GigsVM
+ * 
+ * Test: Homepage, there are input text for search gig
+ * 
+ * Look at:
+ * • _Layout.cshtml
+ * • GigsVM.cs
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
