5e69f4a63803092decbe930cd4ba86a0f335babd
Yosendra <yosendra.evri@gmail.com>
2024-04-01 18:22:54 +0800

    Handling Bootstrap Events - 03. Consuming The API

---
+++ 09. Handling Bootstrap Events/03. Consuming The API/GigHub/Global.asax.cs
@@ -0,0 +1,36 @@
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
+
+/* Password : User1#123
+ * user1@domain.com
+ * yosi@domain.com
+ * artist1@domain.com
+ * artist2@domain.com
+ * 
+ * Consume the api when popover event "shown.bs.popover" is fired.
+ * The code is in _Layout.cshtml
+ * 
+ * (There is still bug, when I click again the notification icon, the popover doesn't appear. Maybe we will fix later (or not?))
+ * 
+ * Look at:
+ * • _Layout.cshtml
+*/
+
+namespace GigHub
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
